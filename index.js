89ecac1125fda8f14e2f9a793ebe3217dc5bfb60
selvakumar22400 <78866668+selvakumar22400@users.noreply.github.com>
2021-08-31 20:13:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const Database = require("./database");
 
 //initialization
 const ourApp = express();
+ourApp.use(express.json());
+//rought      : /
+//description : check the server
+//access      : public
+//method      : GET
+//params      : none
+//body        : none
+
 
 ourApp.get /*http method*/ ("/" ,(request,response ) => {
      //return the response of json format
@@ -19,7 +27,7 @@ ourApp.get /*http method*/ ("/" ,(request,response ) => {
 //body        : none
 
 ourApp.get("/Book",(req,res) => {
-     return res.json({ books:Database.Book });  
+     return res.json({ selva:Database.Book });  
 });
 
 //rought      : /book/:bookID
@@ -31,11 +39,11 @@ ourApp.get("/Book",(req,res) => {
 
 ourApp.get("/Book/:bookID",(req,res) => {
      const getBook = Database.Book.filter(
-          (book) => book.ISBN === req.params.bookID
+          (boo) => boo.ISBN === req.params.bookID
      );
      return res.json({book : getBook});
 });
- 
+  
 //rought      : /book/c/:catagory
 //description : to get a book base on catagory
 //access      : public
@@ -65,7 +73,53 @@ ourApp.get("/Book/a/:authors",(req,res) => {
 //params      : none
 //body        : none
 ourApp.get("/author",(req,res) => {
-     return res.json({Author:Database.Author});
+     return res.json({Authors:Database.Author});
+});
+ 
+//rought : /book/new
+//description : to add a new book
+//access : public
+//method : post
+//params : none
+//body : none
+ourApp.post("/book/new",(req,res) => {
+     const newbook = req.body;
+     Database.Book.push(newbook);
+     return res.json(Database.Book);
+
+});
+//rought : /author/new
+//description : to add a new author
+//access : public
+//method : post
+//params : none
+//body : none
+ourApp.post("/author/new",(req,res) => {
+     const  newAuthor = req.body;
+     Database.Author.push(newAuthor);
+     return res.json(Database.Author);
+});
+
+ourApp.post("/Publication/new",(req,res) => {
+     const Publication = req.body;
+     Database.Author.push(Publication);
+     return res.json({ massage : "new Publication is added"});
+});
+//rought : /book/update
+//description : to update a new book
+//access : public
+//method : put
+//params : none
+//body : none
+ourApp.put("/book/update/:isbn",(req,res) => {
+     const updatedBook = req.body;
+     const isbn = req.params;
+     const getBook = Database.Book.map((book) => 
+     if (book.ISBN === isbnbook.ISBN === isbn) {
+          
+     } ()
+     return (...book ,...updatedBook) );
+
 });
 
 //4000 is port number
